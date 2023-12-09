69aaaf133531782200928c1d0913920eb3fea6d5
Risav pyakurel <77790895+risav-pyakurel@users.noreply.github.com>
2023-12-10 00:05:16 +0545

    Add files via upload

---
+++ JS/02_datatypes.js
@@ -0,0 +1,17 @@
+ "use strict" ; // treat all JS code as a newer version
+
+//  alert(44+67) we are using nodejs not browser
+
+// code readability should be high
+
+// number = 2**53 (power) [ deadline of the num ]
+// biginit
+// string =""
+// boolean  = True / False
+//  null = stand alone value (representation of empty value) , we can assign null
+// undefined = not defining value after declaring variable
+// symbol = unique 
+// object 
+
+console.log(typeof 556); // number
+console.log(typeof "556"); // string
